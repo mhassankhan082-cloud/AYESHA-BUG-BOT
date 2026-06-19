74001615366f7b638f623d7625fdc691f49cb968
mhassankhan082-cloud <mhassankhan082@gmail.com>
2026-06-19 07:40:32 +0500

    AYESHA-MD

--- config.js
+++ config.js
@@ -1,43 +1,43 @@
-global.namaown = "Qasim Ali" // your owner name here
-global.namabot = "© GlobalTechInfo" // your bot name here
+global.namaown = "Ayesha-Md" // your owner name here
+global.namabot = "© Ayesha-Md" // your bot name here
 global.versisc = "9.5.0" //
-global.owner = ["923444844060"] // your owner number here
-global.tele = "https://t.me/GlobalBotInc" // your telegram here
-global.url = "https://www.youtube.com/@GlobalTechInfo" // Your YouTube Here
-global.namastore = "LynnZxD" // UBAH NAMA STORE LU
-global.simbol = "✪" // You Can Change Simbol
-global.wlcm = []
-global.wlcmm = []
+global.owner = ["923170829819"] // your owner number here
+global.tele = "https://t.me/ayeshabibi1111" // your telegram here
+global.url = "https://youtube.com/@ayeshabibi.01?si=sWeh2CTlsP4GKi6P" // Your YouTube Here
+global.namastore = "Ayesha-Md" // UBAH NAMA STORE LU
+global.simbol = "." // You Can Change Simbol
+global.wlcm ="Group me welcome! 🎉"
+global.wlcmm ="Bye bye! Allah hafiz" 
 global.limitawal = {
     premium: "Infinity",
     free: 20
 }
 
 // Ganti Logo Disini
-global.painlogo = "http://telegra.ph/file/9a332c815ad31956d5ce6.jpg" 
+global.painlogo = "https://files.catbox.moe/0vgv8g.jpg" 
 
 global.my = {
-      saluran: "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07",
-      idCH: "120363319098372999@newsletter",
-      youtube: "https://www.youtube.com/@GlobalTechInfo",
-      telegram: "https://t.me/GlobalBotInc",
-      Instagram: "https://www.instagram.com/"
+      saluran: "https://whatsapp.com/channel/0029Vb7qmxVG8l5LP0E9w61H",
+      idCH: , 
+      youtube: "https://youtube.com/@ayeshabibi.01?si=sWeh2CTlsP4GKi6P",
+      telegram: "t.me/ayeshabibi1111",
+      Instagram: "https://www.instagram.com/ayeshabibi011?igsh=Z3J5Z2J6MWtxOHU0"
    }
 
 
 global.mess = {
-    success: 'ᴅᴏɴᴇ',
-    admin: '_*❗ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ !*_',
-    botAdmin: '_*❗ʙᴏᴛ ᴍᴜꜱᴛ ʙᴇ ᴛʜᴇ ᴀᴅᴍɪɴ !*_',
-    OnlyOwner: '_*❗ᴏᴡɴᴇʀꜱ ꜱᴘᴇᴄɪᴀʟ ꜰᴜɴᴄᴛɪᴏɴ !*_',
-    OnlyGrup: '_*❗ꜱᴘᴇᴄɪᴀʟ ꜰᴜɴᴄᴛɪᴏɴ ꜰᴏʀ ɢʀᴏᴜᴘ !*_',
-    private: '_(❗ꜱᴘᴇᴄɪᴀʟ ꜰᴜɴᴄᴛɪᴏɴ ꜰᴏʀ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ !*_',
-    wait: '_*ᴘʀᴏᴄᴇꜱꜱɪɴɢ ᴘʟᴇᴀꜱᴇ ᴡᴀɪᴛ*_',
-    notregist: '_*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ʀᴇɢɪꜱᴛᴇʀᴇᴅ*_',
-    premium: '_*ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴘʀᴇᴍɪᴜᴍ_*',
-    endLimit: '_*Your Daily Limit Has Been Used Up, The Limit Will Be Reset Every 00:00 WIB_*.',
-     bugrespon: `\`[ ! ]\`ᴡᴀɪᴛ ; ᴄᴜʀʀᴇɴᴛʟʏ ꜱᴇɴᴅɪɴɢ ᴠɪʀᴜꜱ ᴀᴛᴛᴀᴄᴋ`,
-     donebug: `\`[ ! ]\`ᴠɪʀᴜꜱ ʜᴀꜱ ʙᴇᴇɴ ꜱᴇɴᴛ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ`,
+  success: 'Ho gaya!',
+  admin: '_* ! Aap admin nahi hain !*_',
+  botAdmin: '_* ! Bot ko pehle group admin banao !*_',
+  OnlyOwner: '_* ! Ye sirf owner ke liye hai !*_',
+  OnlyGrup: '_* ! Ye sirf group me kaam karega !*_',
+  private: '_( ! Ye sirf private chat me kaam karega )_',
+  wait: '_*Zara ruko, process ho raha hai*_',
+  notregist: '_*Aap register nahi hain*_',
+  premium: '_*Aap premium user nahi hain*_',
+  endLimit: '_*Aap ki roz ki limit khatam ho gayi*_',
+  bugrespon: '`\[ ! \]`Thodi der ruko, bheja ja raha hai',
+  donebug: '`\[ ! \]`Kaam ho gaya!'
 }
 
 
